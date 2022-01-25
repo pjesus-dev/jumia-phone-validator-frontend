a93c5c928400db2c53a2a049e036c3dd0e04013e
pjesus-dev <paulosjc.jesus@gmail.com>
2022-01-25 02:44:32 -0300

    adding environment variable for backend url and env sample file

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const API_PREFIX = 'api/v1';
+const API_PATH = `${BACKEND_URL}${API_PREFIX}`;
+
 export default axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: API_PATH,
   headers: {
     "Content-type": "application/json",
   },
